c80c2698c2e7bdef51dd4bca53c16c50f3931311
Ole Kristian Sunde <ole.kristian.sunde@sparebank1.no>
2019-08-22 14:46:06 +0200

    Add test project

---
+++ src/main/java/no/sparebank1/test/DeployToCentralTestApplication.java
@@ -0,0 +1,13 @@
+package no.sparebank1.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployToCentralTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployToCentralTestApplication.class, args);
+	}
+
+}

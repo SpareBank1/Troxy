17547f190a9aa283f091a6e92622c3228162a77c
Ole Kristian Sunde <ole.kristian.sunde@sparebank1.no>
2019-08-22 14:08:15 +0200

    build, then push

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
+docker build -t "$TROXY_DOCKER_IMAGE:$VERSION" .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push "$TROXY_DOCKER_IMAGE:$VERSION"
-docker build -t "$TROXY_DOCKER_IMAGE:$VERSION" .
 

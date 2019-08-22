bfbc43ac4a0ebe7057c9f612be4b062206b15877
Ole Kristian Sunde <ole.kristian.sunde@sparebank1.no>
2019-08-22 14:51:36 +0200

    set up build

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+VERSION=${VERSION:-'local'}
+
+./mvnw install -Drevision="$VERSION"
+
 docker build -t "$TROXY_DOCKER_IMAGE:$VERSION" .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push "$TROXY_DOCKER_IMAGE:$VERSION"

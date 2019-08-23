f0421610d11d5b2321987838af58a23513dbed95
Ole Kristian Sunde <ole.kristian.sunde@sparebank1.no>
2019-08-23 08:19:11 +0200

    improve build.sh error handling

--- build.sh
+++ build.sh
@@ -2,9 +2,29 @@
 
 VERSION=${VERSION:-'local'}
 
-./mvnw deploy -s settings.xml -Drevision="$VERSION"
+mvnDeploy() {
+    ./mvnw deploy -s ./settings.xml -Drevision="$VERSION"
+}
 
-docker build -t "$TROXY_DOCKER_IMAGE:$VERSION" .
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push "$TROXY_DOCKER_IMAGE:$VERSION"
+dockerBuild() {
+    docker build -t "$TROXY_DOCKER_IMAGE:$VERSION" .
+}
+
+dockerLogin() {
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+}
+
+dockerPush() {
+    docker push "$TROXY_DOCKER_IMAGE:$VERSION"
+}
+
+die() {
+    echo "$0: " "$@" >&2
+    exit 1
+}
+
+mvnDeploy || die 'Maven build failure'
+dockerBuild || die 'Docker build failure'
+dockerLogin || die 'Docker login failure'
+dockerPush || die 'Docker push failure'
 
